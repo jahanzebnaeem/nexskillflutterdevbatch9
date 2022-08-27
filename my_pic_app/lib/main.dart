c0a5761f9b4ba4f32dc4d20af2ac86858596cdfe
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-08-27 13:32:22 +0500

    Lecture 3
    Card app design and icon add.

--- my_pic_app/lib/main.dart
+++ my_pic_app/lib/main.dart
@@ -6,43 +6,64 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Text('Home'),
+          title: Text('My Card'),
           backgroundColor: Colors.teal.shade700,
         ),
         body: Column(
           children: [
-            Container(
-              child: Text(
-                'Box 1',
-                style: TextStyle(
-                  fontSize: 22.0,
-                ),
+            CircleAvatar(
+              backgroundImage: AssetImage('images/jahanzeb.png'),
+              radius: 80.0,
+            ),
+            Text(
+              'Jahanzeb Naeem',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
+            ),
+            Text(
+              'Founder | CTO | Flutter Instructor',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
               color: Colors.white,
-              width: 100.0,
-              height: 100.0,
-            ),
-            Container(
-              child: Text(
-                'Box 2',
-                style: TextStyle(
-                  fontSize: 22.0,
+              height: 50.0,
+              margin: EdgeInsets.all(20.0),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 16.0,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '123 456 7890',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              color: Colors.orange,
-              width: 100.0,
-              height: 100.0,
+            ),
+            SizedBox(
+              height: 10.0,
             ),
             Container(
-              child: Text(
-                'Box 3',
-                style: TextStyle(
-                  fontSize: 22.0,
-                ),
-              ),
-              color: Colors.blue,
-              width: 100.0,
-              height: 100.0,
+              color: Colors.white,
+              height: 40.0,
             ),
           ],
         ),
