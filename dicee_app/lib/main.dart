f20848ddc5f7f8e3143428d69903693cb4c57681
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-09-04 13:35:28 +0500

    Lecture 6
    Complete Dicee app.

--- dicee_app/lib/main.dart
+++ dicee_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,20 +16,37 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 8.0,
-              ),
-              child: Image.asset(
-                'images/dice1.png',
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                  // print('Left dice number is: $leftDiceNumber');
+                });
+              },
+              child: Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0,
+                ),
+                child: Image.asset(
+                  'images/dice$leftDiceNumber.png',
+                ),
               ),
             ),
           ),
